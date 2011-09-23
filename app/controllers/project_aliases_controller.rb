a1973e7e528965cf681c2561d069269c314527f6
s-andy <s-andy@f15da11a-c3e1-4864-828a-58d95a4a2cfd>
2011-09-23 12:16:56 +0000

    Implemented adding
    git-svn-id: http://subversion.andriylesyuk.com/project-alias@4 f15da11a-c3e1-4864-828a-58d95a4a2cfd

--- app/controllers/project_aliases_controller.rb
+++ app/controllers/project_aliases_controller.rb
@@ -4,12 +4,24 @@ class ProjectAliasesController < ApplicationController
     before_filter :require_admin
 
     def index
-        @aliases = ProjectAlias.all # FIXME: (:order => :alias)
+        @aliases = ProjectAlias.find(:all) # FIXME: (:order => :alias)
+        @projects = Project.find(:all) # FIXME: visible!
     end
 
     def new
         @alias = ProjectAlias.new
-        @projects = Project.find(:all)
+        @projects = Project.find(:all, :order => 'lft')
+    end
+
+    def create
+        @alias = ProjectAlias.new(params[:project_alias])
+        if @alias.save
+            flash[:notice] = l(:notice_successful_create)
+            redirect_to(:action => 'index')
+        else
+            @projects = Project.find(:all, :order => 'lft')
+            render('project_aliases/new')
+        end
     end
 
 end
--- app/models/project_alias.rb
+++ app/models/project_alias.rb
@@ -3,4 +3,13 @@ class ProjectAlias < ActiveRecord::Base
 
     validates_presence_of :project, :alias
     validates_uniqueness_of :alias
+    validates_length_of :alias, :in => 1..Project::IDENTIFIER_MAX_LENGTH
+    validates_format_of :alias, :with => /^(?!\d+$)[a-z0-9\-]*$/
+    validates_exclusion_of :alias, :in => %w(new)
+
+    def validate
+        errors.add(:same_as_identifier) if self.alias == self.project.identifier # FIXME
+        errors.add(:identifier_exists)  if Project.find_all_by_identifier(self.alias).any? # FIXME
+    end
+
 end
